4145b6e0361ce12e0b25521a1c1f3ffd14bd7ba0
msanchez <msanchez@paxos.com>
2024-11-23 19:07:03 -0300

    Pantalla listado usuarios fi

--- src/app/HomeBankingApp.java
+++ src/app/HomeBankingApp.java
@@ -1,27 +1,40 @@
 package app;
 
+import javax.swing.JOptionPane;
+
 import db.ConnectionManager;
 import db.DatabaseSetup;
 import exception.DatabaseException;
 import service.CuentaBancariaService;
+import service.UsuarioService;
 import ui.PanelManager;
 
 public class HomeBankingApp {
 
 	public static void main(String[] args) {
 		// La primera vez que se usa el proyecto no va a haber cuentas bancarias creadas
-		// Se puede correr la clase CrearCuentasBancarias para crear algunas cuentas por
+		// Se puede correr la clase CrearDatosIniciales para crear algunas cuentas por
 		// primera vez
 		try {
 			DatabaseSetup.inicializarH2DB();
 			DatabaseSetup.crearTablaCuentaBancaria();
 			DatabaseSetup.crearTablaUsuario();
 
-			CuentaBancariaService service = new CuentaBancariaService();
+			CuentaBancariaService cuentaBancariaService = new CuentaBancariaService();
+			UsuarioService usuarioService = new UsuarioService();
 
-			PanelManager manager = new PanelManager(service);
+			PanelManager manager = new PanelManager(cuentaBancariaService, usuarioService);
 			manager.showFrame();
-			manager.mostrarPantallaListadoCuentas();
+			
+			String[] opciones = {"Usuarios", "Cuentas Bancarias"};
+            int eleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción para comenzar:",
+                    "Inicio", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
+
+            if (eleccion == 0) {
+                manager.mostrarPantallaListadoUsuarios();
+            } else {
+                manager.mostrarPantallaListadoCuentas();
+            }
 		} catch (DatabaseException e) {
 			System.err.println("Ocurrió un error: " + e.getMessage() + " - " + e.getCause());
 		} finally {
